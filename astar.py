1ed33f870c18b30c6a77ab83da2a5f3fdc7cae1d
c <omar.ezzt251@gmail.com>
2022-11-29 18:30:45 +0200

    minor modifications

--- astar.py
+++ astar.py
@@ -90,180 +90,182 @@ class Spot:
 
 
 def h(p1, p2):
-	x1, y1 = p1
-	x2, y2 = p2
-	return abs(x1 - x2) + abs(y1 - y2)
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
-path = []
+# path = []
 def reconstruct_path(came_from, current, draw):
-	global path
-	while current in came_from:
-		current = came_from[current]
-		current.make_path()
-		path.append((current.x+15,current.y+15))
-		draw()
+    # global path
+    path = []
+    while current in came_from:
+        current = came_from[current]
+        current.make_path()
+        path.append((current.x+15,current.y+15))
+        draw()
+  
 
 
-def algorithm(draw, grid, start, end):
-	count = 0
-	open_set = PriorityQueue()
-	open_set.put((0, count, start))
-	came_from = {}
-	g_score = {spot: float("inf") for row in grid for spot in row}
-	g_score[start] = 0
-	f_score = {spot: float("inf") for row in grid for spot in row}
-	f_score[start] = h(start.get_pos(), end.get_pos())
+def algorithm(draw, grid, start, end,path):
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count, start))
+    came_from = {}
+    g_score = {spot: float("inf") for row in grid for spot in row}
+    g_score[start] = 0
+    f_score = {spot: float("inf") for row in grid for spot in row}
+    f_score[start] = h(start.get_pos(), end.get_pos())
+    open_set_hash = {start}
 
-	open_set_hash = {start}
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
 
-	while not open_set.empty():
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
 
-		current = open_set.get()[2]
-		open_set_hash.remove(current)
+        if current == end:
+            # path = reconstruct_path(came_from, end, draw)
+            
+            while current in came_from:
+                current = came_from[current]
+                current.make_path()
+                path.append((current.x+15,current.y+15))
+                # draw()
+            path.reverse()
+            end.make_end()
+            return True ,path
+        
 
-		if current == end:
-			reconstruct_path(came_from, end, draw)
-			end.make_end()
-			return True
+        for neighbor in current.neighbors:
+            temp_g_score = g_score[current] + 1
 
-		for neighbor in current.neighbors:
-			temp_g_score = g_score[current] + 1
+            if temp_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g_score
+                f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+                if neighbor not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbor], count, neighbor))
+                    open_set_hash.add(neighbor)
+                    neighbor.make_open()
 
-			if temp_g_score < g_score[neighbor]:
-				came_from[neighbor] = current
-				g_score[neighbor] = temp_g_score
-				f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
-				if neighbor not in open_set_hash:
-					count += 1
-					open_set.put((f_score[neighbor], count, neighbor))
-					open_set_hash.add(neighbor)
-					neighbor.make_open()
+        # draw()
 
-		draw()
+        if current != start:
+            current.make_closed()
 
-		if current != start:
-			current.make_closed()
-
-	return False
-
+    return False,path
 
 def make_grid(rows, width):
-	grid = []
-	gap = width // rows
-	for i in range(rows):
-		grid.append([])
-		for j in range(rows):
-			spot = Spot(i, j, gap, rows)
-			grid[i].append(spot)
-
-	return grid
-
+    return [[Spot(i, j, width // rows, rows) for j in range(rows)] for i in range(rows)]
 
 def draw_grid(win, rows, width):
-	gap = width // rows
-	for i in range(rows):
-		pygame.draw.line(win, BLACK, (0, i * gap), (width, i * gap))
-		for j in range(rows):
-			pygame.draw.line(win, BLACK, (j * gap, 0), (j * gap, width))
+    for y in range(rows):pygame.draw.line(win, RED, (0, y * width // rows), (width, y * width // rows))
+    for x in range(rows):pygame.draw.line(win, RED, (x * width // rows, 0), (x * width // rows, width))
 
 
-def draw(win, grid, rows, width,car):
-	win.fill(BLACK)
+def draw(win, grid, rows, width,car,path):
+    win.fill(BLACK)
 
-	for row in grid:
-		for spot in row:
-			spot.draw(win)
-	draw_grid(win, rows, width)
-	pygame.image.save(win,"map.png")
-	car.update_map(pygame.image.load('map.png'))
-	car.draw_Car()
-	car.draw_radar()
-	pygame.display.update()
+    for row in grid:
+        for spot in row:
+            spot.draw(win)
+    # draw_grid(win, rows, width)
+    pygame.image.save(win,"map.png")
+    car.update_map(pygame.image.load('map.png'))
+    car.draw_path(path)
+    car.draw_Car()
+    car.draw_radar()
+    pygame.display.update()
 
 
 def get_clicked_pos(pos, rows, width):
 	gap = width // rows
-	y, x = pos
-
-	row = y // gap
-	col = x // gap
-
-	return row, col
+	return pos[0] // gap, pos[1] // gap
 
 
 def main(win, width):
-	ROWS = 20
-	grid = make_grid(ROWS, width)
-	car = Car(win,pygame.image.load('map.png'))
-	point = None
-	start = None
-	end = None
-	global path
-	# path.reverse()
+    ROWS = 20
+    grid = make_grid(ROWS, width)
+    car = Car(win,pygame.image.load('map.png'))
+    point = None
+    start = None
+    end = None
+    # global path
+    # path.reverse()
+    path = []
+    run = True
+    while run:
+        draw(win, grid, ROWS, width,car,path)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-	run = True
-	while run:
-		draw(win, grid, ROWS, width,car)
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				run = False
+            if pygame.mouse.get_pressed()[0]: # LEFT
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                spot = grid[row][col]
+                r,c =get_clicked_pos((int(car.source[0]),int(car.source[1])), ROWS, width)
+                startspot = grid[r][c]
+                start = startspot
+                start.make_start()
+                if not start and spot != end:
+                    pass
 
-			if pygame.mouse.get_pressed()[0]: # LEFT
-				pos = pygame.mouse.get_pos()
-				row, col = get_clicked_pos(pos, ROWS, width)
-				spot = grid[row][col]
-				if not start and spot != end:
-					start = spot
-					start.make_start()
+                elif not end and spot != start:
+                    path.clear()
+                    end = spot
+                    end.make_end()
 
-				elif not end and spot != start:
-					end = spot
-					end.make_end()
+                # elif event.type == pygame.KEYDOWN:
+                #     if event.key == pygame.K_SPACE :
+                #         spot.make_barrier()
 
-				elif spot != end and spot != start:
-					spot.make_barrier()
+            elif pygame.mouse.get_pressed()[2]: # RIGHT
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                spot = grid[row][col]
+                spot.reset()
+                if spot == start:
+                    start = None
+                elif spot == end:
+                    end = None
+            elif pygame.mouse.get_pressed()[1]: # RIGHT
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                spot = grid[row][col]
+                spot.make_barrier()
 
-			elif pygame.mouse.get_pressed()[2]: # RIGHT
-				pos = pygame.mouse.get_pos()
-				row, col = get_clicked_pos(pos, ROWS, width)
-				spot = grid[row][col]
-				spot.reset()
-				if spot == start:
-					start = None
-				elif spot == end:
-					end = None
+            if event.type == pygame.KEYDOWN:
+                # if event.key == pygame.K_SPACE :
+                #     make_barrier = True
+                #     spot.make_barrier()
+                if event.key == pygame.K_c :
+                        path.clear()
+                        point = None
+                        start = None
+                        end = None
+                        grid = make_grid(ROWS, width)
+                        car.reached = True
+                        
+            if start and end:
+                for row in grid:
+                    for spot in row:
+                        spot.update_neighbors(grid)
 
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_SPACE and start and end:
-					for row in grid:
-						for spot in row:
-							spot.update_neighbors(grid)
+                _,path = algorithm(lambda: draw(win, grid, ROWS, width,car), grid, start, end ,path)
+                end = None
+                print(len(path))
 
-					algorithm(lambda: draw(win, grid, ROWS, width,car), grid, start, end)
+                
 
-				if event.key == pygame.K_c:
-					path.clear()
-					point = None
-					start = None
-					end = None
-					grid = make_grid(ROWS, width)
-					car.reached = True
-		# if event.key == pygame.K_x:
-		if car.reached:
-			point = car.get_steps(0,path)
-		# 	print(point)
-		# 	car.reached = False
-		# if len(path) == 0:
-		# 	car.reached = True
-		car.prepare_car()
-		car.set_destination(point)
-		print(path,point,car.reached)
-		# car.move()
-		
+        if car.reached:
+            point = car.get_steps(0,path)
+        
+        car.prepare_car()
+        car.set_destination(point)
+        # print(path,point,car.reached,len(path))
 
-	pygame.quit()
+    pygame.quit()
 
 main(WIN, WIDTH)
